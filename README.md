7f1732d0585e08655e6ba2fbd11c3c1130db5344
MrLuffyMom <aekbotepranav25@gmail.com>
2023-10-21 20:09:34 +0530

    Updated version to 1.2.2

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # traffic-lights
-Ver 1.1.1:
-Wrote a basic code to start camera and detect the objects stated in coco.names using yolov3, opencv and numpy
-Currently just detects the objects and marks them with a title and a box  
-Ver 1.2.1:
-Improved upon the written code to count the number of cars from the video and returns the real time count in the terminal  
+  
+## Required Files
+Download yolo3.weights from <a href="https://pjreddie.com/media/files/yolov3.weights" download>here.</a>  
+Download coco.names <a href="blob:https://github.com/516335cd-7c8e-4628-a551-27b05452edf8" download>here.</a>  
+Download yolo3.cfg <a href="blob:https://github.com/f42f5760-69c3-4e9a-957e-0e1c39d297ca" download>here.</a>  
+  
+## Required Python Modules:
+ 1.opencv-python
+ 2.numpy  
+  
+## How to Run?
+ Install the neccessary modules and download the required files.  
+ Download the object_detector.py and run it.
+
+### Ver 1.1.1:
+Wrote a basic code to start camera and detect the objects stated in coco.names using yolov3, opencv and numpy.  
+Currently just detects the objects and marks them with a title and a box.  
+  
+### Ver 1.2.1:
+Improved upon the written code to count the number of cars from the video and returns the real time count in the terminal.  
+  
+### Ver 1.2.2:
+Improved upon the written code to ONLY count the number of cars from the video and put the code to do so in a class so that it is reusable.  
--- object_detector.py
+++ object_detector.py
@@ -1,62 +1,74 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-whT = 320
-confThreshold = 0.5
-nmsThreshold = 0.3
+class Camera:
+    def __init__(self, camera_id, names_path, config_path, weights_path, conf_threshold=0.5, nms_threshold=0.3):
+        self.camera_id = camera_id
+        self.confThreshold = conf_threshold
+        self.nmsThreshold = nms_threshold
 
-classesFile = '/home/pranav/Downloads/Traffic/traffic-lights-bitches/coco.names'
-classNames = []
-with open(classesFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+        self.classesFile = names_path
+        self.classNames = []
+        with open(self.classesFile, 'rt') as f:
+            self.classNames = f.read().rstrip('\n').split('\n')
 
-modelConfiguration = '/home/pranav/Downloads/Traffic/traffic-lights-bitches/yolov3.cfg'
-modelWeights = '/home/pranav/Downloads/Traffic/traffic-lights-bitches/yolov3.weights'
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+        self.modelConfiguration = config_path
+        self.modelWeights = weights_path
+        self.net = cv2.dnn.readNetFromDarknet(self.modelConfiguration, self.modelWeights)
+        self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-def findObjects(outputs, img):
-    hT, wT, cT = img.shape
-    bbox = []
-    classIds = []
-    confs = []
-    for output in outputs:
-        for det in output:
-            scores = det[5:]
-            classId = np.argmax(scores)
-            confidence = scores[classId]
-            if confidence > confThreshold:
-                w, h = int(det[2] * wT), int(det[3] * hT)
-                x, y = int((det[0] * wT) - w/2), int((det[1] * hT) - h/2)
-                bbox.append([x, y, w, h])
-                classIds.append(classId)
-                confs.append(float(confidence))
-    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
-    car_count = 0
-    for i in indices:
-        box = bbox[i]
-        x, y, w, h = box[0], box[1], box[2], box[3]
-        if classNames[classIds[i]].lower() == 'car':
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-            cv2.putText(img, f'Car {int(confs[i]*100)}%', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
-            car_count += 1
+    def process_frame(self, frame):
+        hT, wT, cT = frame.shape
+        blob = cv2.dnn.blobFromImage(frame, 1/255, (320, 320), [0, 0, 0], 1, crop=False)
+        self.net.setInput(blob)
+        layerNames = self.net.getLayerNames()
+        outputLayerIndices = self.net.getUnconnectedOutLayers()
+        outputNames = [layerNames[i-1] for i in outputLayerIndices]
+        outputs = self.net.forward(outputNames)
 
-    return img, car_count
+        bbox = []
+        classIds = []
+        confs = []
+        for output in outputs:
+            for det in output:
+                scores = det[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+                if confidence > self.confThreshold:
+                    w, h = int(det[2] * wT), int(det[3] * hT)
+                    x, y = int((det[0] * wT) - w/2), int((det[1] * hT) - h/2)
+                    bbox.append([x, y, w, h])
+                    classIds.append(classId)
+                    confs.append(float(confidence)
+        )
 
-while True:
-    success, img = cap.read()
-    blob = cv2.dnn.blobFromImage(img, 1/255, (whT, whT), [0, 0, 0], 1, crop=False)
-    net.setInput(blob)
-    layerNames = net.getLayerNames()
-    outputLayerIndices = net.getUnconnectedOutLayers()
-    outputNames = [layerNames[i-1] for i in outputLayerIndices]
-    outputs = net.forward(outputNames)
+        indices = cv2.dnn.NMSBoxes(bbox, confs, self.confThreshold, self.nmsThreshold)
+        car_count = 0
+        for i in indices:
+            box = bbox[i]
+            x, y, w, h = box[0], box[1], box[2], box[3]
+            if self.classNames[classIds[i]].lower() == 'car':
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
+                cv2.putText(frame, f'Car {int(confs[i]*100)}%', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+                car_count += 1
 
-    img, car_count = findObjects(outputs, img)
+        return frame, car_count
 
-    print(f'Number of cars: {car_count}')
+    def process_video(self):
+        cap = cv2.VideoCapture(self.camera_id)
+        while True:
+            success, frame = cap.read()
+            if not success:
+                break
+            frame, car_count = self.process_frame(frame)
+            print(f'Camera {self.camera_id}: Number of cars: {car_count}')
+            cv2.imshow(f'Camera {self.camera_id}', frame)
+            cv2.waitKey(1)
 
-    cv2.imshow('Image', img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    # Example usage for camera 0
+    camera0 = Camera(0, '/home/pranav/Downloads/Traffic/traffic-lights-bitches/coco.names',
+                     '/home/pranav/Downloads/Traffic/traffic-lights-bitches/yolov3.cfg',
+                     '/home/pranav/Downloads/Traffic/traffic-lights-bitches/yolov3.weights')
+    camera0.process_video()
